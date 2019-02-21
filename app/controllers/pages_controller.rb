a78b63ac38a99d27a66f4b1c9cc5001875d718d2
Melanie Schmitz-Jansen <melanieschmitzj@gmail.com>
2019-02-21 18:12:46 +0100

    changes pages controller, bookings (create, delete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,15 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @users = User.all.sample(6)
+    @users = User.joins(:services).sample(6)
   end
 
   def user_index
-    @users = User.all
+    @users = User.joins(:services)
   end
 
   def user_show
     @user = User.find(params[:id])
   end
+
 end
